2aa846f0370b0830a08bddad197a05dae4ccdae2
MukulPichunia-Fexle <mukul.pichunia@fexle.com>
2024-10-22 17:08:52 +0530

    Main Code

---
+++ classes/Student.java
@@ -0,0 +1,9 @@
+class Student {
+    private int id;
+    private String name;
+    public static void main(String[] args) {
+        Student st = new Student()l
+
+        System.out.println(st.name);
+    }
+}
